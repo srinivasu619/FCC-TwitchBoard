224fd6e9ffe51b923f07a52f6086ed22f77af054
srinivasu619 <srinivasu.618@gmail.com>
2017-08-07 17:56:39 +0530

    Changed some Earlier Scripting

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-var top_streamers = ['syndicate','riotgames','summit1g','esl_csgo','nightblue3','imaqtpie','lirik','sodapoppin'];
+var top_streamers = ['syndicate','esl_csgo','riotgames','summit1g','nightblue3','imaqtpie','lirik'];
 
 $(document).ready(function(){
    console.log("document ready");
   for(var i=0;i<top_streamers.length;i++)
     {
-      call(top_streamers[i])
+      call(top_streamers[i],i);
     }
 });
-function call(streamer)
+function call(streamer,i)
 {
-  //console.log('function');
-   $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/"+streamer+"?callback=?",function(response){
-     console.log(response);
-    // console.log(response.display_name);
-    // console.log(response.logo);
-    // console.log(response._links.self);
-  });
+   $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/"+streamer+"?callback=?").then(function(result){
+     var url = 'https://www.twitch.tv/'+streamer;
+     if(result.stream !== null)
+      {
+        //console.log(i);
+        
+        $('#status'+i).html('<span class="online">ONLINE</span><a class="noDeco" target="_blank" href='+url+'><i class="fa fa-twitch fa-2x" aria-hidden="true"></i></a>');
+        $('#game'+i).html('\uD83D\uDD34 LIVE: '+result.stream.channel.game);
+      }
+     else
+       {
+         $('#status'+i).html('<span class="offline">OFFLINE</span><a class="noDeco" target="_blank" href='+url+'><i class="fa fa-twitch fa-2x" aria-hidden="true"></i></a>');
+       }
+   });
 }
